f5f3fcd8796ae19317320f103a09b9d06239ef13
Salvador Rioboo Naranjo <salvador.rioboo@mayoral.com>
2023-07-18 00:57:43 +0200

    feat: added markdown blog example

--- sv-blog/README.md
+++ sv-blog/README.md
@@ -36,3 +36,7 @@ npm run build
 You can preview the production build with `npm run preview`.
 
 > To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+
+## Markdown blog example
+
+Example from https://joyofcode.xyz/sveltekit-markdown-blog
--- sv-blog/src/routes/Header.svelte
+++ sv-blog/src/routes/Header.svelte
@@ -19,6 +19,9 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/blog' ? 'page' : undefined}>
+				<a href="/blog">Blog</a>
+			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>

682468be8423ef595d0a358f61eef56da3d0bd78
Salvador Rioboo Naranjo <salvador.rioboo@mayoral.com>
2023-08-04 21:02:27 +0200

    feat(sv-storyblok): base files

---
+++ sv-storyblok/src/routes/+layout.ts
@@ -0,0 +1,28 @@
+import Feature from '../components/Feature.svelte';
+import Grid from '../components/Grid.svelte';
+import Page from '../components/Page.svelte';
+import Teaser from '../components/Teaser.svelte';
+import { apiPlugin, storyblokInit, useStoryblokApi } from '@storyblok/svelte';
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load() {
+    storyblokInit({
+        accessToken: 'HERE YOUR ACCESS TOKEN',
+        use: [apiPlugin],
+        components: {
+            feature: Feature,
+            grid: Grid,
+            page: Page,
+            teaser: Teaser
+        },
+        https: true,
+        apiOptions: {
+            region: ''
+        }
+    });
+    let storyblokApi = await useStoryblokApi();
+
+    return {
+        storyblokApi: storyblokApi
+    };
+}
